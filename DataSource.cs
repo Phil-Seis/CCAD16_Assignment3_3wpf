8a7a5d42e8636d3c983a7e3f3e1fa1162d19b92a
Philip Seisman <Philip.seisman@gmail.com>
2024-11-25 15:33:56 -0500

    Add project files.

---
+++ DataSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCAD16_Assignment3_2_2wpf
+{
+    internal class DataSource
+    {
+        public static List<Student> Students;
+
+        public static void CreateData()
+        {
+            Students = new List<Student>()
+            {
+                new Student() {StudentId = 1, FirstName = "Philip", LastName = "Seisman", Address = "228 Sycamore St.", MonthOfAdmission = Month.January, Grade = 'A' },
+                new Student() {StudentId = 2, FirstName = "Chris", LastName = "Smith", Address = "1 Antelope Cir.", MonthOfAdmission = Month.January, Grade = 'A' },
+                new Student() {StudentId = 3, FirstName = "Jasmine", LastName = "George", Address = "13 NW Arnold St.", MonthOfAdmission = Month.January, Grade = 'A' }
+            };
+        }
+    }
+}
+    
---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCAD16_Assignment3_2_2wpf
+{
+    public enum Month
+    {
+        January, February, March, April, May, June, July, August, September, October, November, December
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public Month MonthOfAdmission { get; set; }
+        public char Grade { get; set; }
+
+        public void AddStudent(Student student)
+        {
+            DataSource.Students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            DataSource.Students.Remove(student);
+        }
+    }
+}
